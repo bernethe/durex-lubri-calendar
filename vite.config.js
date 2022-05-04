04aa3292961e9914661ada0ab21ec1631d89f828
Harold Bernethe Soto González <bernethe@gmail.com>
2022-05-03 23:06:43 -0600

    Open Graph image

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  root: './',
-    build: {
-        outDir: './build'
-  },
+  plugins: [react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/og.png',
+          dest: '.'
+        }
+      ]
+    })
+  ],
   resolve: {
     alias: {
       '@': '/src/',
     },
+  },
+  root: './',
+    build: {
+        outDir: './build'
   }
 })
